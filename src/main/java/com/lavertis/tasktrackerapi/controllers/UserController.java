dc50e2c5018f113ffd95c2054b30c81f7ea94947
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-03-26 00:32:41 +0100

    feat: add exception handler (#3)

--- src/main/java/com/lavertis/tasktrackerapi/controllers/UserController.java
+++ src/main/java/com/lavertis/tasktrackerapi/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
 import com.lavertis.tasktrackerapi.dto.CreateUserRequest;
 import com.lavertis.tasktrackerapi.entities.User;
+import com.lavertis.tasktrackerapi.exceptions.NotFoundException;
 import com.lavertis.tasktrackerapi.services.user_service.IUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,20 +31,19 @@ public class UserController {
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<User> findById(@PathVariable long id) {
+    public ResponseEntity<User> findById(@PathVariable long id) throws NotFoundException {
         var user = userService.findById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PostMapping
-    @ExceptionHandler
     public ResponseEntity<User> create(@Valid @RequestBody CreateUserRequest request) {
         var user = userService.create(request);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @PatchMapping(value = "{id}", consumes = "application/json-patch+json")
-    public ResponseEntity<User> updateById(@PathVariable long id, @RequestBody JsonPatch patch) throws JsonPatchException, JsonProcessingException {
+    public ResponseEntity<User> updateById(@PathVariable long id, @RequestBody JsonPatch patch) throws JsonPatchException, JsonProcessingException, NotFoundException {
         var user = userService.updateById(id, patch);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
--- src/main/java/com/lavertis/tasktrackerapi/services/user_service/IUserService.java
+++ src/main/java/com/lavertis/tasktrackerapi/services/user_service/IUserService.java
@@ -5,17 +5,18 @@ import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
 import com.lavertis.tasktrackerapi.dto.CreateUserRequest;
 import com.lavertis.tasktrackerapi.entities.User;
+import com.lavertis.tasktrackerapi.exceptions.NotFoundException;
 
 import java.util.List;
 
 public interface IUserService {
     List<User> findAll();
 
-    User findById(long id);
+    User findById(long id) throws NotFoundException;
 
     User create(CreateUserRequest request);
 
-    User updateById(long id, JsonPatch patch) throws JsonPatchException, JsonProcessingException;
+    User updateById(long id, JsonPatch patch) throws JsonPatchException, JsonProcessingException, NotFoundException;
 
     void deleteById(long id);
 }
