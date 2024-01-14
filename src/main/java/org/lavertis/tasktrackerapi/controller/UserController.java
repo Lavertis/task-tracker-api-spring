1774e587d0bcc7419ceedcfa973ab8295001eaca
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2024-01-14 21:22:23 +0100

    feat: add swagger requirements and operation summary to user controller

--- src/main/java/org/lavertis/tasktrackerapi/controller/UserController.java
+++ src/main/java/org/lavertis/tasktrackerapi/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.lavertis.tasktrackerapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.lavertis.tasktrackerapi.dto.user.CreateUserRequest;
 import org.lavertis.tasktrackerapi.dto.user.UpdateUserRequest;
 import org.lavertis.tasktrackerapi.service.user_service.IUserService;
@@ -17,21 +19,28 @@ public class UserController {
     private IUserService userService;
 
     @PostMapping()
+    @Operation(summary = "User sign up")
     public ResponseEntity<?> createUser(@RequestBody CreateUserRequest createUserRequest) {
         return ResponseEntity.ok(userService.createUser(createUserRequest));
     }
 
     @GetMapping("/current")
+    @SecurityRequirement(name = "bearerAuth")
+    @Operation(summary = "Get current user")
     public ResponseEntity<?> getCurrentUser(Principal principal) {
         return ResponseEntity.ok(userService.getUserByUsername(principal.getName()));
     }
 
     @PatchMapping("/current")
+    @SecurityRequirement(name = "bearerAuth")
+    @Operation(summary = "Update current user")
     public ResponseEntity<?> updateCurrentUser(@RequestBody UpdateUserRequest updateUserRequest, Principal principal) {
         return ResponseEntity.ok(userService.updateUser(principal.getName(), updateUserRequest));
     }
 
     @DeleteMapping("/current")
+    @SecurityRequirement(name = "bearerAuth")
+    @Operation(summary = "Delete current user")
     public ResponseEntity<?> deleteCurrentUser(Principal principal) {
         return ResponseEntity.ok(userService.deleteUser(principal.getName()));
     }
