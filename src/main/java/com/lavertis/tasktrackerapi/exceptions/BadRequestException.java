2b7719cf366e875849d2edea6e773fe894d1f285
Lavertis <29688908+Lavertis@users.noreply.github.com>
2024-06-16 00:26:02 +0200

    refactor: remove code leftovers

--- src/main/java/com/lavertis/tasktrackerapi/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.lavertis.tasktrackerapi.exceptions;
-
-public class BadRequestException extends ApiRequestException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
